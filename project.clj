4aab34f5b0aa93dc9b27e78e0f4e90b531c14916
Pierre Radermecker <pradermecker@yahoo.ca>
2012-05-30 15:07:58 +0200

    Initial commit : first kiss and raw data schema.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject wi3ne "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [
+                 [org.clojure/clojure "1.4.0"]
+                 [com.datomic/datomic "0.1.3157"]
+                 ])
---
+++ test/wi3ne/test/core.clj
@@ -0,0 +1,6 @@
+(ns wi3ne.test.core
+  (:use [wi3ne.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
