04ec9e5b63b2c9f1d873598567f45b2e7167f884
PierreR <pradermecker@yahoo.ca>
2012-08-31 20:33:41 +0200

    update datamic version

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject wi3ne "1.0.0-SNAPSHOT"
+(defproject wine "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [
-                 [org.clojure/clojure "1.4.0"]
-                 [com.datomic/datomic "0.1.3157"]
-                 ])
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.datomic/datomic-free "0.8.3470"]])
