fa30d99b5cad04bcefea3c501ca646995f8435b9
PierreR <pradermecker@yahoo.ca>
2012-09-02 11:39:03 +0200

    update datomic version

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.datomic/datomic-free "0.8.3470"]])
+                 [com.datomic/datomic-free "0.8.3488"]])
